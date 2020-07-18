c9fe12b624cb568c5bfe08b80694c087f59d7cac
cezklosowski <cez.klosowski@gmail.com>
2020-07-18 11:52:02 +0200

    making map

--- src/main/java/sda/VoivodshipMapper.java
+++ src/main/java/sda/VoivodshipMapper.java
@@ -1,5 +1,6 @@
 package sda;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
@@ -7,9 +8,21 @@ import java.util.stream.Collectors;
 
 public class VoivodshipMapper {
     public static Map<Integer, String> mapData(List<String> loadedRawData) {
-        List<String> voivodeshipLines = loadedRawData.stream() // wrzucenie listy do streama
+
+        Map<Integer, String> voivodeshipMap = new HashMap<>();
+
+        loadedRawData.stream() // wrzucenie listy do streama
                 .filter(line -> line.contains("województwo")) // filtrowanie z wykorzystaniem lambdy
-                .collect(Collectors.toList()); // zrzucenie streama z powrotem do listy
-        return null;
+                .forEach(filteredLine -> { // przejście po wszystkich
+
+                    String[] splitLines = filteredLine.split(";"); // splitowanie
+
+                    Integer number = Integer.valueOf(splitLines[0]); // wyciągnięcie numeru województwa
+                    String name = splitLines[4]; // wyciągnięcie nazwy województwa
+
+                    voivodeshipMap.put(number, name); // wrzucenie elementu do mapy = numer - klucz, nazwa - wartość
+                });
+
+        return voivodeshipMap;
     }
 }
