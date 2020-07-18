d93487eb9b78a7b9961ae0ac05f16466270a02d7
cezklosowski <cez.klosowski@gmail.com>
2020-07-18 14:03:40 +0200

    Random location generator with tests

--- src/main/java/sda/VacationTargetGenerator.java
+++ src/main/java/sda/VacationTargetGenerator.java
@@ -1,5 +1,27 @@
 package sda;
 
+import sda.model.Location;
+import sda.model.LocationType;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class VacationTargetGenerator {
 
+
+    public static Location getRandomLocation(List<Location> locations) {
+        int randomNumber = new Random().nextInt(locations.size());
+        return locations.get(randomNumber);
+    }
+
+    public static Location getRandomLocation(List<Location> locations, LocationType wantedType) {
+        List<Location> filteredLocations = locations.stream()
+                .filter(loc -> loc.getType() == wantedType)
+                .collect(Collectors.toList());
+        return getRandomLocation(filteredLocations);
+
+    }
 }
+
+
---
+++ src/test/java/sda/VacationTargetGeneratorTest.java
@@ -0,0 +1,40 @@
+package sda;
+
+import org.junit.Test;
+import sda.model.*;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class VacationTargetGeneratorTest {
+
+    private List<Location> locationsForTest = List.of(
+            new Village("Hobbiton","Shire"),
+            new Village("Wola Zaorana","Testowe"),
+            new City("Gdańsk","Pomorskie"),
+            new UrbanRural("I have no idea", "Dunno")
+    );
+
+    @Test
+    public void shouldReturnSomething(){
+
+        // when
+        Location randomLocation = VacationTargetGenerator.getRandomLocation(locationsForTest);
+
+        // then
+        assertNotNull(randomLocation);
+    }
+
+    @Test
+    public void shouldReturnType(){
+
+        // when
+        Location randomLocation = VacationTargetGenerator.getRandomLocation(locationsForTest, LocationType.CITY);
+
+        // then
+        assertEquals(LocationType.CITY, randomLocation.getType());
+
+    }
+
+}
