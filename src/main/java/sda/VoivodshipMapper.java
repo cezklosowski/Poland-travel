f10100b3c62ac040328c9eea5cf8773207a62722
cezklosowski <cez.klosowski@gmail.com>
2020-07-18 11:35:11 +0200

    filtering map with lambda

--- src/main/java/sda/VoivodshipMapper.java
+++ src/main/java/sda/VoivodshipMapper.java
@@ -8,12 +8,7 @@ import java.util.stream.Collectors;
 public class VoivodshipMapper {
     public static Map<Integer, String> mapData(List<String> loadedRawData) {
         List<String> voivodeshipLines = loadedRawData.stream() // wrzucenie listy do streama
-                .filter(new Predicate<String>() { // przefiltrowanie danych, które spełniają dany warunek filtrowania
-                    @Override // anonimowa implementacja klasy Predicate
-                    public boolean test(String s) {
-                        return s.contains("województwo");
-                    }
-                })
+                .filter(line -> line.contains("województwo")) // filtrowanie z wykorzystaniem lambdy
                 .collect(Collectors.toList()); // zrzucenie streama z powrotem do listy
         return null;
     }
