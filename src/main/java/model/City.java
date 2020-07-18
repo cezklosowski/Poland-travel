ad750df0298749e38a5d53e012c141b1874dbc30
cezklosowski <cez.klosowski@gmail.com>
2020-07-18 09:55:18 +0200

    Initial commit

---
+++ src/main/java/model/City.java
@@ -0,0 +1,13 @@
+package model;
+
+public class City extends Location {
+
+    public City(String name, String voivodeship) {
+        super(name, voivodeship, LocationType.CITY);
+    }
+
+    @Override
+    public String sayGreeting() {
+        return "Welcome to big, dynamic " + getName();
+    }
+}
---
+++ src/main/java/model/UrbanRural.java
@@ -0,0 +1,13 @@
+package model;
+
+public class UrbanRural extends Location {
+
+    public UrbanRural(String name, String voivodeship) {
+        super(name, voivodeship, LocationType.URBAN_RURAL);
+    }
+
+    @Override
+    public String sayGreeting() {
+        return "Welcome to totally balanced " + getName();
+    }
+}
